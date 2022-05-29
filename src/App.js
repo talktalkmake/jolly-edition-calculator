f7f43972e44037107991d279df93c5f3d21152ab
Tom Hart <kitsonian@gmail.com>
2022-05-29 08:25:32 -0400

    Updating HTML classes to use tailwindcss

--- src/App.js
+++ src/App.js
@@ -8,31 +8,31 @@ import PrintPricing from './components/PrintPricing'
 function App() {
   const [weddingDate, setWeddingDate] = useState(false)
   const dateObj = calculateDueDates(weddingDate)
-  
+
   return (
     <>
-    <img src={process.env.PUBLIC_URL + '/je-logo.svg'} alt="Jolly Edition logo" className="je-logo mt-2" />
-    <h1 className="text-center text-6xl">Calcudate</h1>
-    <section className="row">
-      <label htmlFor="wedding-date" className="block">When is the wedding day?</label>
-      <input
-        type="date"
-        min={moment().add(3, 'months').format('YYYY-MM-DD')}
-        max={moment().add(2, 'years').format('YYYY-MM-DD')}
-        id="wedding-date"
-        onChange={e => setWeddingDate(e.target.value)} />
-    </section>
-    {weddingDate && (
-    <section className="row events">
-    {dateObj.map(event => 
-      <dl className={event.impossible ? 'strike ghost' : null} key={getId()}>
-          <dt className="event-label"><span className="block h3">{event.label}</span> due date</dt>
-          <dd className="event-date">{event.date ? event.date : 'not possible (date in the past)'}</dd>
-      </dl>
-    )}
-    </section>
-    )}
-    <PrintPricing />
+      <img src={process.env.PUBLIC_URL + '/je-logo.svg'} alt="Jolly Edition logo" className="block mx-auto w-32" />
+      <h1 className="text-center text-4xl mt-2">Calcudate</h1>
+      <section className="text-center mt-8">
+        <label htmlFor="wedding-date" className="block">When is the wedding day?</label>
+        <input
+          type="date"
+          min={moment().add(3, 'months').format('YYYY-MM-DD')}
+          max={moment().add(2, 'years').format('YYYY-MM-DD')}
+          id="wedding-date"
+          onChange={e => setWeddingDate(e.target.value)} />
+      </section>
+      {weddingDate && (
+        <section className="text-center events">
+          {dateObj.map(event =>
+            <dl className={event.impossible ? 'strike ghost' : null} key={getId()}>
+              <dt className="event-label"><span className="block h3">{event.label}</span> due date</dt>
+              <dd className="event-date">{event.date ? event.date : 'not possible (date in the past)'}</dd>
+            </dl>
+          )}
+        </section>
+      )}
+      <PrintPricing />
     </>
   );
 }
--- src/components/PrintPricing.jsx
+++ src/components/PrintPricing.jsx
@@ -5,93 +5,93 @@ import formatOptions from '../reducers/formatOptions'
 import getListOfFormatOptions from '../functions/getListOfFormatOptions'
 
 const embellishments = {
-    foil: {costPerPrint: 3},
-    letterpress: {costPerPrint: 3},
-    edgePainting: {costPerPrint: 1}, 
-    addressPrint: {costPerPrint: 3},
+    foil: { costPerPrint: 3 },
+    letterpress: { costPerPrint: 3 },
+    edgePainting: { costPerPrint: 1 },
+    addressPrint: { costPerPrint: 3 },
 }
 
-function PrintPricing(){
+function PrintPricing() {
     const [state, dispatch] = useReducer(reducer, initialState)
     let total = 0
 
     const calculateSubtotal = (item, quantity) => {
         let subTotal = 0
         subTotal += formatOptions[item.format].costPerPrint
-        if ( item.envelope )
+        if (item.envelope)
             subTotal += .5
-        if ( item.addressPrint )
+        if (item.addressPrint)
             subTotal += item.addressPrint === '1-side' ? (embellishments.addressPrint.costPerPrint) : (embellishments.addressPrint.costPerPrint * 2)
-        if ( item.hasStamp )
+        if (item.hasStamp)
             subTotal += .58
-        if ( item.foil )
+        if (item.foil)
             subTotal += embellishments.foil.costPerPrint
-        if ( item.letterpress )
+        if (item.letterpress)
             subTotal += embellishments.letterpress.costPerPrint
-        if ( item.edgePainting )
+        if (item.edgePainting)
             subTotal += embellishments.edgePainting.costPerPrint
-        if ( item.thickness > 1 )
+        if (item.thickness > 1)
             subTotal += ((item.thickness * formatOptions[item.format].costPerPrint) - formatOptions[item.format].costPerPrint)
         total += (subTotal * quantity)
         return subTotal
     }
 
-    return(
-        <>
-        <section className="row">
-            <h1 className="text-center text-6xl">Print Pricing</h1>
+    return (
+        <section className="text-center">
+            <p className="mt-8">§</p>
+            <h1 className="text-center text-4xl mt-2">Print Pricing</h1>
             <table className="mt-2">
                 <tbody>
-                {state.map((product, i) => (
-                    <tr key={i}>
-                        <td><input
-                            type="text"
-                            value={product.name}
-                            className="product-name"
-                            onChange={e => dispatch({type: "updateProductName", id: i, name: e.target.value})}
+                    {state.map((product, i) => (
+                        <tr key={i}>
+                            <td><input
+                                type="text"
+                                value={product.name}
+                                className="product-name"
+                                onChange={e => dispatch({ type: "updateProductName", id: i, name: e.target.value })}
                             />
-                        </td>
-                        <td><select
-                            defaultValue={product.format}
-                            onChange={e => dispatch({type: 'changeFormat', id: i, format: e.target.value})}>
+                            </td>
+                            <td><select
+                                defaultValue={product.format}
+                                onChange={e => dispatch({ type: 'changeFormat', id: i, format: e.target.value })}>
                                 {getListOfFormatOptions(Object.keys(formatOptions), product.format)}
                             </select>
-                        </td>
-                        <td>
-                            <input type="checkbox" defaultChecked={product.envelope} onChange={() => dispatch({type: 'toggleEnvelope', id: i})} />
-                        </td>
-                        <td>{product.envelope &&
-                            <input type="checkbox" defaultChecked={product.hasStamp} onChange={e => dispatch({type: "toggleStamp", id: i})} />
-                        }
-                        </td>
-                        <td>{product.envelope && 
-                            <select defaultValue={product.addressPrint} onChange={e => dispatch({type: 'changeAddressPrint', id: i, addressPrint: e.target.value})}>
-                                <option>None</option>
-                                <option>1-side</option>
-                                <option>2-side</option>
-                            </select>}</td>
-                        <td><input type="checkbox" defaultChecked={product.foil} onChange={() => dispatch({type: 'toggleFoil', id: i})} /></td>
-                        <td><input type="checkbox" defaultChecked={product.letterpress} onChange={() => dispatch({type: 'toggleLetterpress', id: i})} /></td>
-                        <td><input type="checkbox" defaultChecked={product.edgePainting} onChange={e => dispatch({type: 'toggleEdgePainting', id: i})} /></td>
-                        <td>
-                            {!product.fold &&
-                            <select defaultValue={product.thickness} onChange={e => dispatch({type: 'changeThickness', thickness: parseInt(e.target.value), id: i})}>
-                                <option>1</option>
-                                <option>2</option>
-                                <option>3</option>
-                            </select>
+                            </td>
+                            <td>
+                                <input type="checkbox" defaultChecked={product.envelope} onChange={() => dispatch({ type: 'toggleEnvelope', id: i })} />
+                            </td>
+                            <td>{product.envelope &&
+                                <input type="checkbox" defaultChecked={product.hasStamp} onChange={e => dispatch({ type: "toggleStamp", id: i })} />
                             }
-                        </td>
-                        <td>
-                            <input
-                            type="number"
-                            onChange={e => dispatch({type: 'updateQuantity', id: i, quantity: parseInt(e.target.value)})}
-                            defaultValue={product.quantity}
-                            className="quantity--inline" />
-                        </td>
-                        <td>${calculateSubtotal(state[i], product.quantity)}</td>
-                    </tr>)
-                )}
+                            </td>
+                            <td>{product.envelope &&
+                                <select defaultValue={product.addressPrint} onChange={e => dispatch({ type: 'changeAddressPrint', id: i, addressPrint: e.target.value })}>
+                                    <option>None</option>
+                                    <option>1-side</option>
+                                    <option>2-side</option>
+                                </select>}</td>
+                            <td><input type="checkbox" defaultChecked={product.foil} onChange={() => dispatch({ type: 'toggleFoil', id: i })} /></td>
+                            <td><input type="checkbox" defaultChecked={product.letterpress} onChange={() => dispatch({ type: 'toggleLetterpress', id: i })} /></td>
+                            <td><input type="checkbox" defaultChecked={product.edgePainting} onChange={e => dispatch({ type: 'toggleEdgePainting', id: i })} /></td>
+                            <td>
+                                {!product.fold &&
+                                    <select defaultValue={product.thickness} onChange={e => dispatch({ type: 'changeThickness', thickness: parseInt(e.target.value), id: i })}>
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                    </select>
+                                }
+                            </td>
+                            <td>
+                                <input
+                                    type="number"
+                                    onChange={e => dispatch({ type: 'updateQuantity', id: i, quantity: parseInt(e.target.value) })}
+                                    defaultValue={product.quantity}
+                                    className="quantity--inline" />
+                            </td>
+                            <td>${calculateSubtotal(state[i], product.quantity)}</td>
+                        </tr>)
+                    )}
                 </tbody>
                 <thead>
                     <tr>
@@ -108,25 +108,9 @@ function PrintPricing(){
                         <th>Total</th>
                     </tr>
                 </thead>
-                <tfoot>
-                    <tr>
-                        <th>Name</th>
-                        <th>Format</th>
-                        <th>Envelope</th>
-                        <th>Stamp</th>
-                        <th>Address<br />Print</th>
-                        <th>Foil</th>
-                        <th>Letter<br />press</th>
-                        <th>Edge<br />painting</th>
-                        <th>Thickness</th>
-                        <th>Sets</th>
-                        <th>Total</th>
-                    </tr>
-                </tfoot>
             </table>
-            <h1 className="text-center text-6xl">${total.toLocaleString()}</h1>
+            <h1 className="text-center text-4xl mt-2">${total.toLocaleString()}</h1>
         </section>
-        </>
     )
 }
 
