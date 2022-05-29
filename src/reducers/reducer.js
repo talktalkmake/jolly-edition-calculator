d1b97296292faec8c26732187752dcc03ade660d
Tom Hart <kitsonian@gmail.com>
2022-05-29 09:20:40 -0400

    Format on save

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -7,7 +7,7 @@ function reducer(state, action) {
 
   switch (action.type) {
     case 'changeFormat':
-      temp = {...state[action.id]}
+      temp = { ...state[action.id] }
       temp.format = action.format
       // update the fold attrib.
       temp.fold = formatOptions[temp.format].hasFold
@@ -15,39 +15,39 @@ function reducer(state, action) {
       return [...state]
 
     case 'changeAddressPrint':
-      temp = {...state[action.id]}
-      temp.addressPrint = ( action.addressPrint === 'None' )
+      temp = { ...state[action.id] }
+      temp.addressPrint = (action.addressPrint === 'None')
         ? false
         : action.addressPrint
       state[action.id] = temp
       return [...state]
 
-    case 'toggleStamp': 
-      temp = {...state[action.id]}
+    case 'toggleStamp':
+      temp = { ...state[action.id] }
       temp.hasStamp = !temp.hasStamp
       state[action.id] = temp
       return [...state]
 
     case 'toggleFoil':
-      temp = {...state[action.id]}
+      temp = { ...state[action.id] }
       temp.foil = !temp.foil
       state[action.id] = temp
       return [...state]
 
     case 'toggleLetterpress':
-      temp = {...state[action.id]}
+      temp = { ...state[action.id] }
       temp.letterpress = !temp.letterpress
       state[action.id] = temp
       return [...state]
 
     case 'changeThickness':
-      temp = {...state[action.id]}
+      temp = { ...state[action.id] }
       temp.thickness = action.thickness
       state[action.id] = temp
       return [...state]
 
     case 'toggleEdgePainting':
-      temp = {...state[action.id]}
+      temp = { ...state[action.id] }
       temp.edgePainting = !temp.edgePainting
       state[action.id] = temp
       return [...state]
@@ -67,7 +67,7 @@ function reducer(state, action) {
       return [...state]
 
     case 'updateQuantity':
-      temp = {...state[action.id]}
+      temp = { ...state[action.id] }
       temp.quantity = action.quantity
       state[action.id] = temp
       return [...state]
