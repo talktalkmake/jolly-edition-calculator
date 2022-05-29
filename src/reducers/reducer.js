1a2b53fe4f40affbc5bf4d39379305cfc5712f8f
Tom Hart <kitsonian@gmail.com>
2022-05-29 09:08:41 -0400

    Remove extraneous return

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,7 +1,7 @@
 import formatOptions from './formatOptions'
 
 function reducer(state, action) {
-  
+
   state = [...state]
   let temp
 
@@ -12,25 +12,22 @@ function reducer(state, action) {
       // update the fold attrib.
       temp.fold = formatOptions[temp.format].hasFold
       state[action.id] = temp
-      return state
-      
       return [...state]
+
     case 'changeAddressPrint':
       temp = {...state[action.id]}
       temp.addressPrint = ( action.addressPrint === 'None' )
         ? false
         : action.addressPrint
       state[action.id] = temp
-      return state
       return [...state]
 
     case 'toggleStamp': 
       temp = {...state[action.id]}
       temp.hasStamp = !temp.hasStamp
       state[action.id] = temp
-      return state
-    
       return [...state]
+
     case 'toggleFoil':
       temp = {...state[action.id]}
       temp.foil = !temp.foil
@@ -42,41 +39,37 @@ function reducer(state, action) {
       temp = {...state[action.id]}
       temp.letterpress = !temp.letterpress
       state[action.id] = temp
-      return state
       return [...state]
 
     case 'changeThickness':
       temp = {...state[action.id]}
       temp.thickness = action.thickness
       state[action.id] = temp
-      return state
-      
       return [...state]
+
     case 'toggleEdgePainting':
       temp = {...state[action.id]}
       temp.edgePainting = !temp.edgePainting
       state[action.id] = temp
-      return state
       return [...state]
 
     case 'toggleEnvelope':
       temp = {...state[action.id]}
       temp.envelope = !temp.envelope
       state[action.id] = temp
-      return state
       return [...state]
 
     case 'updateProductName':
       temp = {...state[action.id]}
       temp.name = action.name
       state[action.id] = temp
-      return state
+      return [...state]
 
     case 'updateQuantity':
       temp = {...state[action.id]}
       temp.quantity = action.quantity
       state[action.id] = temp
-      return state
+      return [...state]
 
     default:
       throw new Error();
